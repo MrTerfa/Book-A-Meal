4fdb20b283bca6c75101d5d40e7115730224e218
Terfa Olamide Tortema <terfa.tortema@gmail.com>
2018-04-30 12:09:53 +0100

    setup files

--- src/app.js
+++ src/app.js
@@ -12,7 +12,47 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/',(req,res) => {
-    res.send('Hello World');
+	res.send("Hello to Book a meal");
 })
+app.get('/meals', (req, res) => {
+	res.send(meals);
+});
 
-app.listen(3000,() =>{console.log("App is running on port 3000!!!")});
+app.post('/meals', (req,res) => {
+	const meal = {
+		id : meals.length + 1,
+		name : req.body.name
+	};
+	meals.push(meal);
+	res.send(meal);
+});
+
+app.put('/meals/:mealid', (req,res) => {
+	req.post()
+});
+app.delete('/meals/:mealid',(req,res) => {
+	req.delete()
+});
+
+
+app.post('/menu/',(req,res) => {
+	req.post()
+});
+app.get('/menu/',(req,res) => {
+	req.post()
+});
+
+app.post('/orders',(req,res) => {
+	req.post()
+});
+app.put('/orders/:orderid', (req,res) => {
+	req.post()
+});
+app.get('/orders',(req,res) => {
+	req.post()
+});
+
+
+//PORT 
+const port = process.env.PORT || 3000
+app.listen(port, () => console.log(`App is listening on ${port}`))
